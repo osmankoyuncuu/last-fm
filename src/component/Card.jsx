4b011abb494fa1bf0b8f22a67d7f4a92b085b545
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2022-12-19 00:56:36 +0300

    add

---
+++ src/component/Card.jsx
@@ -0,0 +1,36 @@
+const Card = ({ image, name, listeners, playcount }) => {
+  return (
+    <div className="border border-black w-full flex px-2 py-4 gap-3 bg-lime-100">
+      <img src={image[2]["#text"]} alt="" width={"75px"} />
+      <div className="flex justify-between gap-3 w-full">
+        <div className="w-1/3">
+          <h5>Artist</h5>
+          <div className="border border-b-black"></div>
+          <h2 className="font-bold flex flex-wrap">{name}</h2>
+        </div>
+        <div className="flex justify-center flex-col text-black text-sm">
+          <div className="flex gap-1">
+            <div>
+              <p>listeners</p>
+              <p>playcount</p>
+            </div>
+            <div>
+              <p>
+                {listeners > 1000000
+                  ? `: ${(listeners / 1000000).toFixed(1)} M`
+                  : `: ${(listeners / 1000).toFixed(1)} K`}
+              </p>
+              <p>
+                {playcount > 1000000
+                  ? `: ${(playcount / 1000000).toFixed(1)} M`
+                  : `: ${(playcount / 1000).toFixed(1)} K`}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Card from "../component/Card";
+import Header from "../component/Header";
+import {
+  fetchError,
+  fetchStart,
+  fetchSuccess,
+} from "../features/topArtistsSlice";
+
+const Home = () => {
+  const { topArtists } = useSelector((state) => state.lastfm);
+  const dispatch = useDispatch();
+  const getTopArtists = async () => {
+    dispatch(fetchStart());
+    try {
+      const topArtistUrl = `https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=8e73114473fcefc5ba2848a79a622848&format=json`;
+      const { data } = await axios.get(topArtistUrl);
+      const {
+        artists: { artist },
+      } = data;
+      dispatch(fetchSuccess(artist));
+    } catch (error) {
+      dispatch(fetchError());
+    }
+  };
+  console.log(topArtists);
+  useEffect(() => {
+    getTopArtists();
+  }, []);
+
+  return (
+    <div className="flex items-center justify-center flex-col w-screen h-screen opacity-90">
+      <Header header={"Top Artist List"} />
+      <div className="bg-lime-200 mt-3 w-96 h-2/3 flex flex-col border border-black rounded-md p-2 gap-2 overflow-auto">
+        {topArtists?.map((artist, index) => (
+          <Card {...artist} key={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
