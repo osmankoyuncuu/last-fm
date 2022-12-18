2e21760ff3ea69716fdf49054c43d088491af336
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2022-12-19 00:55:48 +0300

    add

---
+++ src/component/Header.jsx
@@ -0,0 +1,11 @@
+import Card from "./Card";
+
+const Header = ({ header }) => {
+  return (
+    <div className="w-48 h-10 bg-slate-400 flex justify-center items-center rounded-md">
+      <h2 className="text-white text-center">{header}</h2>
+    </div>
+  );
+};
+
+export default Header;
