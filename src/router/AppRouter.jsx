0413c043029f56365b7d483af59806370057b5c4
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2022-12-19 16:26:25 +0300

    add navbar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "../component/Navbar";
 import Details from "../pages/Details";
 import Home from "../pages/Home";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/details" element={<Details />} />
+        <Route path="/details/:name" element={<Details />} />
       </Routes>
     </BrowserRouter>
   );
